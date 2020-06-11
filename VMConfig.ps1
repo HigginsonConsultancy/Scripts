5058feabc66eae7179e009f2b28cff65cbd1e073
HigginsonConsultancy <52958405+HigginsonConsultancy@users.noreply.github.com>
2020-06-11 14:47:57 +0100

    Update VMConfig.ps1

--- VMConfig.ps1
+++ VMConfig.ps1
@@ -4,6 +4,7 @@
     $EventlogSource = "VM Build Script"
     New-EventLog -LogName $EventlogName -Source $EventlogSource
     Limit-EventLog -OverflowAction OverWriteAsNeeded -MaximumSize 64KB -LogName $EventlogName
+    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting VM Build Script"
     }
     
     Catch
@@ -13,6 +14,7 @@
 # Download the source media
 Try
     {
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Attempting to download media"
     $url="https://github.com/HigginsonConsultancy/Media/raw/master/Orca.zip"
     $output="c:\Temp\Orca.zip"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
@@ -25,6 +27,7 @@ Try
 
 Try
     {
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting to expand media to correct location"
     Expand-Archive -LiteralPath $output -DestinationPath C:\Temp\
     }
     
@@ -35,6 +38,7 @@ Try
 
 Try
     {
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting Application Install"
     $Argument3 = "/i " + [char]34 + "C:\Temp\Orca\Orca-x86_en-us.msi" + [char]34 + " /qb"
     Start-Process -FilePath msiexec.exe -ArgumentList $Argument3 -Wait
         }
@@ -44,6 +48,7 @@ Try
     Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Error -Message "Unable to install application"
     }
 
+EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "VM Build Script Completed"
 
 
 
