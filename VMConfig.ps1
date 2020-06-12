fdd9c27e3ba5aeb65ea25a08b42ceda6ed6fa5af
HigginsonConsultancy <52958405+HigginsonConsultancy@users.noreply.github.com>
2020-06-12 12:35:07 +0100

    Update VMConfig.ps1

--- VMConfig.ps1
+++ VMConfig.ps1
@@ -4,7 +4,7 @@
     $EventlogSource = "VM Build Script"
     New-EventLog -LogName $EventlogName -Source $EventlogSource
     Limit-EventLog -OverflowAction OverWriteAsNeeded -MaximumSize 64KB -LogName $EventlogName
-    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting VM Build Script"
+    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "Starting VM Build Script"
     }
     
     Catch
@@ -14,7 +14,7 @@
 # Download the source media
 Try
     {
-    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Attempting to download media"
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "Attempting to download media"
     $url="https://github.com/HigginsonConsultancy/Media/raw/master/Orca.zip"
     $output="c:\Windows\Temp\Orca.zip"
     (New-Object System.Net.WebClient).DownloadFile($url, $output)
@@ -22,33 +22,33 @@ Try
     
     Catch
     {
-    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Error -Message $error[0].Exception
+    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
     }
 
 Try
     {
-    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting to expand media to correct location"
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "Starting to expand media to correct location"
     Expand-Archive -LiteralPath $output -DestinationPath C:\Windows\Temp\
     }
     
     Catch
     {
-    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Error -Message $error[0].Exception
+    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
     }
 
 Try
     {
-    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "Starting Application Install"
+    EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "Starting Application Install"
     $Argument3 = "/i " + [char]34 + "C:\Windows\Temp\Orca\Orca-x86_en-us.msi" + [char]34 + " /qb"
     Start-Process -FilePath msiexec.exe -ArgumentList $Argument3 -Wait
         }
     
     Catch
     {
-    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Error -Message $error[0].Exception
+    Write-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Error -Message $error[0].Exception
     }
 
-EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101  -EntryType Information -Message "VM Build Script Completed"
+EventLog -LogName $EventlogName -Source $EventlogSource -EventID 25101 -EntryType Information -Message "VM Build Script Completed"
 
 
 
